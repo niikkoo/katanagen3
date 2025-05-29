6b3e8cc7a9408ff0310b0ec37fc4fd40363cbfa0
Nicolas <niko30127@gmail.com>
2025-05-29 02:26:26 +0200

    Add files via upload

---
+++ utilities/constant.js
@@ -0,0 +1,29 @@
+﻿//
+//  constant.js
+//
+//  Copyright 2016 Roland Corporation. All rights reserved.
+//
+
+var
+	STX		= 'F0',
+	ROLAND	= '41',
+	RQ1		= '11',
+	DT1		= '12',
+	EOX		= 'F7';
+
+var
+	SYSEX_MAXLEN = 128;
+
+var
+	INTEGER1x1 = 0x10000, // 0000 000a
+	INTEGER1x2 = 0x10001, // 0000 00aa
+	INTEGER1x3 = 0x10002, // 0000 0aaa
+	INTEGER1x4 = 0x10003, // 0000 aaaa
+	INTEGER1x5 = 0x10004, // 000a aaaa
+	INTEGER1x6 = 0x10005, // 00aa aaaa
+	INTEGER1x7 = 0x10006, // 0aaa aaaa
+	INTEGER2x4 = 0x10007, // 0000 aaaa, 0000 bbbb
+	INTEGER2x7 = 0x1000B, // 0aaa aaaa, 0bbb bbbb
+	INTEGER4x4 = 0x10008, // 0000 aaaa, 0000 bbbb, 0000 cccc, 0000 dddd
+	PADDING    = 0x20000; // PADDING | bytes
+
---
+++ utilities/converter.js
@@ -0,0 +1,55 @@
+﻿//
+//  converter.js
+//
+//  Copyright 2016 Roland Corporation. All rights reserved.
+//
+
+	function nibble(x) {
+		return (
+			((x & 0x7f000000) >>> 3) |
+			((x & 0x007f0000) >>> 2) |
+			((x & 0x00007f00) >>> 1) |
+			((x & 0x0000007f))
+		);
+	}
+
+	function nibble_le(x) {
+		return nibble(
+			((x & 0x7f000000) >>> 24) |
+			((x & 0x007f0000) >>>  8) |
+			((x & 0x00007f00) <<   8) |
+			((x & 0x0000007f) <<  24)
+		);
+	}
+
+	function _7bitize(x) {
+		return (
+			((x & 0x0fe00000) << 3) |
+			((x & 0x001fc000) << 2) |
+			((x & 0x00003f80) << 1) |
+			((x & 0x0000007f))
+		);
+	}
+
+	function hex2(x) {
+		var s = x.toString(16).toUpperCase();
+		return (x < 0x10) ? '0' + s : s;
+	}
+
+	function _bid(bid, i) {
+		var t = bid.split('%');
+		if (i === undefined) {
+			t[0] = t[0].replace(/\([0-9]+\)$/g, ''); /* remove part or librarian row */
+		} else {
+			t[0] += '(' + (i + 1) + ')'; /* append part or librarian row */
+		}
+		return t.join('%');
+	}
+
+	function _toUpperCase(s) {
+		if ('i' !== 'I'.toLowerCase()) {
+			// Language setting is Turkey
+			return s.replace(/[a-z]/g, function(ch) {return String.fromCharCode(ch.charCodeAt(0) & ~32);});
+		}
+		return s.toUpperCase();
+	}
